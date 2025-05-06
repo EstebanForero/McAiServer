5fae5b59ada1447393d7abeb2a227ee8aa110c2b
Esteban Forero <estebanmff@outlook.com>
2025-05-06 11:26:28 -0500

    Test code v1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use std::io;
+
+use tokio::{io::AsyncReadExt, net::TcpSocket};
+
+#[tokio::main]
+async fn main() -> io::Result<()> {
+    let addr = "127.0.0.1:8080".parse().unwrap();
+
+    let socket = TcpSocket::new_v4()?;
+    let mut stream = socket.connect(addr).await?;
+
+    let mut buffer = [0; 1500];
+
+    while stream.read(&mut buffer).await.is_ok() {
+        println!("{buffer:?}");
+    }
+
+    Ok(())
 }
